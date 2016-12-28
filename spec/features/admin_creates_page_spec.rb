9ce076e5f702d6876072bc78667958eec3af2c63
spicy <travis@impossiblecreature.com>
2016-12-28 11:42:37 -0700

    fix stylesheet issue

--- spec/features/admin_creates_page_spec.rb
+++ spec/features/admin_creates_page_spec.rb
@@ -1,6 +1,17 @@
 require "rails_helper"
 
 feature "Admin manages pages" do
+  scenario "admin views existing pages" do
+
+    
+    visit "/admin/pages"
+
+    #click_link "New Page"
+
+    #expect(page).to have_text("Page was successfully created.")
+    expect(page).to have_css("h1", text: "Pages")
+  end
+
   scenario "admin creates a page" do
     visit "/admin/pages"
 
